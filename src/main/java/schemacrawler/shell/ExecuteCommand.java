668b35f23c11dce47b20d5f15b224b54cd38dde3
Sualeh Fatehi <sualeh@hotmail.com>
2018-09-11 22:30:25 -0400

    Adding list of commands

--- src/main/java/schemacrawler/shell/ExecuteCommand.java
+++ src/main/java/schemacrawler/shell/ExecuteCommand.java
@@ -1,35 +1,30 @@
 /*
-========================================================================
-SchemaCrawler
-http://www.schemacrawler.com
-Copyright (c) 2000-2018, Sualeh Fatehi <sualeh@hotmail.com>.
-All rights reserved.
-------------------------------------------------------------------------
-
-SchemaCrawler is distributed in the hope that it will be useful, but
-WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-
-SchemaCrawler and the accompanying materials are made available under
-the terms of the Eclipse Public License v1.0, GNU General Public License
-v3 or GNU Lesser General Public License v3.
-
-You may elect to redistribute this code under any of these licenses.
-
-The Eclipse Public License is available at:
-http://www.eclipse.org/legal/epl-v10.html
-
-The GNU General Public License v3 and the GNU Lesser General Public
-License v3 are available at:
-http://www.gnu.org/licenses/
-
-========================================================================
-*/
+ * ======================================================================== SchemaCrawler
+ * http://www.schemacrawler.com Copyright (c) 2000-2018, Sualeh Fatehi <sualeh@hotmail.com>. All
+ * rights reserved. ------------------------------------------------------------------------
+ *
+ * SchemaCrawler is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * SchemaCrawler and the accompanying materials are made available under the terms of the Eclipse
+ * Public License v1.0, GNU General Public License v3 or GNU Lesser General Public License v3.
+ *
+ * You may elect to redistribute this code under any of these licenses.
+ *
+ * The Eclipse Public License is available at: http://www.eclipse.org/legal/epl-v10.html
+ *
+ * The GNU General Public License v3 and the GNU Lesser General Public License v3 are available at:
+ * http://www.gnu.org/licenses/
+ *
+ * ========================================================================
+ */
 
 package schemacrawler.shell;
 
 
 import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.validation.constraints.NotNull;
 
@@ -43,6 +38,7 @@ import org.springframework.shell.standard.ShellOption;
 
 import schemacrawler.schemacrawler.SchemaCrawlerOptions;
 import schemacrawler.schemacrawler.SchemaRetrievalOptions;
+import schemacrawler.tools.executable.CommandRegistry;
 import schemacrawler.tools.executable.SchemaCrawlerExecutable;
 import schemacrawler.tools.options.OutputOptions;
 import schemacrawler.tools.options.OutputOptionsBuilder;
@@ -58,6 +54,19 @@ public class ExecuteCommand
 
   @Autowired
   private SchemaCrawlerShellState state;
+
+  @ShellMethod(value = "List available SchemaCrawler commands", prefix = "-")
+  public void commands()
+    throws Exception
+  {
+    final CommandRegistry registry = new CommandRegistry();
+    final List<String> supportedCommands = new ArrayList<>(registry
+      .getSupportedCommands());
+    for (final String command: supportedCommands)
+    {
+      System.out.println(command);
+    }
+  }
 
   @ShellMethod(value = "Execute a SchemaCrawler command", prefix = "-")
   public void execute(@ShellOption @NotNull final String command)
