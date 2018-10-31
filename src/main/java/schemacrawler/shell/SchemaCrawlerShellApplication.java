880e07be6b19896f108eacdc58e47280217b14db
Sualeh Fatehi <sualeh@hotmail.com>
2018-10-30 23:17:21 -0400

    Simplify

--- src/main/java/schemacrawler/shell/SchemaCrawlerShellApplication.java
+++ src/main/java/schemacrawler/shell/SchemaCrawlerShellApplication.java
@@ -31,14 +31,11 @@ package schemacrawler.shell;
 import org.jline.utils.AttributedString;
 import org.jline.utils.AttributedStyle;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.shell.jline.PromptProvider;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude = { ThymeleafAutoConfiguration.class })
 public class SchemaCrawlerShellApplication
 {
 
